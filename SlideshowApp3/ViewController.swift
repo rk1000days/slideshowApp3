2ea7f70085a83ee0abea0009a9e719753d51eec8
rk1000days <rk1000days@icloud.com>
2020-02-16 21:07:08 +0900

    Initial Commit

---
+++ SlideshowApp3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideshowApp3
+//
+//  Created by Apple on 2020/02/16.
+//  Copyright © 2020 kazuhiro.kabashima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
