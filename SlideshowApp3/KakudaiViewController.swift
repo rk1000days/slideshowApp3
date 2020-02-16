733a30872d54d4cdbe199674855c0818ffee5590
rk1000days <rk1000days@icloud.com>
2020-02-17 00:07:47 +0900

    残り４エラー、４警告。

---
+++ SlideshowApp3/KakudaiViewController.swift
@@ -0,0 +1,51 @@
+//
+//  KakudaiViewController.swift
+//  SlideshowApp3
+//
+//  Created by Apple on 2020/02/16.
+//  Copyright © 2020 kazuhiro.kabashima. All rights reserved.
+//
+
+import UIKit
+
+class KakudaiViewController: UIViewController {
+
+    
+    @IBOutlet weak var kakudaiImage: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        kakudaiImage.image = image!
+    }
+    
+        //画像を拡大する
+    //        let resize = CGSize(image.size.width*3.0,image.size.height*3.0)
+    //        UIGraphicsBeginImageContext(resize)
+    //        image.drawInRect(CGRect(0, 0, resize.width, resize.height))
+    //        let reimage = UIGraphicsGetImageFromCurrentImageContext()
+    //        UIGraphicsEndImageContext()
+    //
+    //        let clip = CGRectMake(
+    //            resize.width - image.frame.size.width/2.0,
+    //            resize.height - image.frame.size.height/2.0,
+    //            image.frame.size.width,
+    //            image.frame.size.height)
+    //        let cgimage = CGImageCreateWithImageInRect(reimage.CGImage, clip)
+    //        image.contentMode = UIView.ContentMode.Center
+    //        image.image = UIImage(CGImage: cgimage!)
+    
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- SlideshowApp3/ViewController.swift
+++ SlideshowApp3/ViewController.swift
@@ -10,11 +10,84 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBAction func onTapImage(_ sender: Any) {
+        performSegue(withIdentifier: "result", sender: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let kakudaiViewController:KakudaiViewController = segue.destination as! KakudaiViewController
+        kakudaiViewController.image = imageView.image
+        timer.invalidate()
+    }
+    
+    var indexNum = 0
+    
+    var timer: Timer!
+    
+    
+    let photoArray = [UIImage(named: "National Studium") ,UIImage(named: "Oita Dome") ,UIImage(named: "Kumamoto Studium")]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let theImage = photoArray[0]
+        imageView.image = theImage
     }
 
-
+//進む
+    @IBOutlet weak var fwd: UIButton!
+    @IBAction func fwdTap(_ sender: Any) {
+        if indexNum >= 3{
+            indexNum = 0
+        }else{
+            indexNum += 1
+        }
+        imageView.image = photoArray[indexNum]
+    }
+    
+//戻る
+    @IBOutlet weak var back: UIButton!
+    @IBAction func backTap(_ sender: Any) {
+        if indexNum <= -1 {
+            indexNum = 2
+        }else{
+            indexNum -= 1
+        }
+        imageView.image = photoArray[indexNum]
+    }
+//
+    @objc func imagetimer(_ timer: Timer) {
+          if self.indexNum == 2 {
+              self.indexNum = 0
+          } else {
+              self.indexNum += 1
+          }
+          self.imageView.image = photoArray[indexNum]
+      }
+   //「再生／停止」ボタン
+    @IBOutlet weak var `switch`: UIButton!
+    //「停止」ボタンを押す
+    @IBAction func switchTap(_ sender: Any) {
+        if self.timer != nil {
+            self.timer.invalidate()
+            self.timer = nil
+            switch.setTitle("再生", for: UIControl.State())
+                fwd.isEnabled = true
+                back.isEnabled = true
+    
+            //「再生」ボタンを押す
+        }else{
+            self.timer = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(imagetimer(_:)) , userInfo: nil, repeats: true)
+                       switch.setTitle("停止", for: UIControl.State())
+                       fwd.isEnabled = false
+                       back.isEnabled = false
+        }
+    
+    
 }
-
+    @IBAction func unwind(_ segue:UIStoryboardSegue){
+       }
+}
